108fa7b0b26c15540bf23bdac9f38fd70b8c1320
beaven <beaven_wang@163.com>
2017-12-21 17:38:15 +0800

    验证rxjava订阅执行顺序

--- app/src/main/java/com/beaven/daggerstudy/presenter/news/NewsPresenter.java
+++ app/src/main/java/com/beaven/daggerstudy/presenter/news/NewsPresenter.java
@@ -7,9 +7,12 @@ import com.beaven.daggerstudy.model.DataManager;
 import com.beaven.daggerstudy.model.bean.NewsData;
 import com.beaven.daggerstudy.model.bean.NewsResult;
 import com.beaven.daggerstudy.model.net.codes.ApiSubscriber;
+import io.reactivex.functions.Action;
+import io.reactivex.functions.Consumer;
 import java.util.List;
 import javax.inject.Inject;
 import javax.inject.Named;
+import org.reactivestreams.Subscription;
 
 /**
  * @author : Beaven
@@ -17,44 +20,57 @@ import javax.inject.Named;
  */
 
 public class NewsPresenter extends BasePresenter<NewsContract.View>
-    implements NewsContract.Presenter {
+        implements NewsContract.Presenter {
 
-  public static final String NEWS_TYPE_NAME = "news_type";
+    public static final String NEWS_TYPE_NAME = "news_type";
 
-  private NewsResultAdapter adapter;
-  private DataManager dataManager;
-  private String type;
+    private NewsResultAdapter adapter;
+    private DataManager dataManager;
+    private String type;
 
-  @Inject
-  public NewsPresenter(@NonNull NewsContract.View baseView, NewsResultAdapter adapter,
-      @Named(NEWS_TYPE_NAME) String type, DataManager dataManager) {
-    super(baseView);
-    this.adapter = adapter;
-    this.type = type;
-    this.dataManager = dataManager;
-  }
+    @Inject
+    public NewsPresenter(@NonNull NewsContract.View baseView, NewsResultAdapter adapter,
+            @Named(NEWS_TYPE_NAME) String type, DataManager dataManager) {
+        super(baseView);
+        this.adapter = adapter;
+        this.type = type;
+        this.dataManager = dataManager;
+    }
 
-  @Override
-  public void onCreate() {
-    super.onCreate();
-    baseView.setAdapter(adapter);
-    loadNews();
-  }
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        baseView.setAdapter(adapter);
+        loadNews();
+    }
 
-  @Override
-  public void loadNews() {
-    disposable.add(dataManager.getNews(type).subscribeWith(new ApiSubscriber<NewsResult>(baseView) {
-      @Override
-      public void onNext(NewsResult newsResult) {
-        super.onNext(newsResult);
-        List<NewsData> dataList = newsResult.getNewsDataList();
-        if (dataList == null || dataList.isEmpty()) {
-          return;
-        }
-        adapter.getDataList().clear();
-        adapter.getDataList().addAll(dataList);
-        adapter.notifyDataSetChanged();
-      }
-    }));
-  }
+    @Override
+    public void loadNews() {
+        disposable.add(dataManager.getNews(type)
+                .doOnSubscribe(subscription -> {
+                    System.out.println("doOnSubscribe");
+                })
+                .doFinally(() -> System.out.println("doFinally"))
+                .doOnComplete(() -> System.out.println("doOnComplete"))
+                .subscribeWith(new ApiSubscriber<NewsResult>(baseView) {
+                    @Override
+                    protected void onStart() {
+                        super.onStart();
+                        System.out.println("OnStart");
+                    }
+
+                    @Override
+                    public void onNext(NewsResult newsResult) {
+                        super.onNext(newsResult);
+                        System.out.println("onNext");
+                        List<NewsData> dataList = newsResult.getNewsDataList();
+                        if (dataList == null || dataList.isEmpty()) {
+                            return;
+                        }
+                        adapter.getDataList().clear();
+                        adapter.getDataList().addAll(dataList);
+                        adapter.notifyDataSetChanged();
+                    }
+                }));
+    }
 }
